0197d6f54ad589413f0acb6e3005c57eab375847
Nitesh J <jnitesh1463@gmail.com>
2024-11-22 00:26:45 +0530

    updated to cors

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -122,6 +122,7 @@ const Home = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
+        mode: 'no-cors',
         credentials: 'include',
       });
 
